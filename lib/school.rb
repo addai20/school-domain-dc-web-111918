5edc5c31d4d8f169ad71fafa8592c7955d1e9ef1
Benjamin Addai <addai.5@buckeyemail.osu.edu>
2018-11-26 17:39:27 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor(:name, :roster)
+  
+  
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(new_student, grade)
+    @roster[grade] = []
+    @roster[grade] << new_student
+  
+  end
+  
+  def grade(roster)
+  
+  end
+  
+  # def sort()
+    
+  # end
+  
+  
+end
